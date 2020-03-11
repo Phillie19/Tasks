f072951b5ad803032be5055262cfc2c63448ef3b
Philipp Pereverzev <61697616+Phillie19@users.noreply.github.com>
2020-03-11 11:23:13 +0300

    Add files via upload

---
+++ How Many Numbers Are Smaller Than the Current Number No1.cpp
@@ -0,0 +1,15 @@
+Solution #1
+class Solution {
+public:
+	vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+		multiset <int> sol;
+		vector <int> out;
+		for (int i = 0; i < nums.size(); i++) {
+			sol.insert(nums[i]);
+		}
+		for (int i = 0; i < nums.size(); i++) {
+			out.push_back(distance(sol.begin(), sol.lower_bound(nums[i])));
+		}
+		return out;
+	}
+};
