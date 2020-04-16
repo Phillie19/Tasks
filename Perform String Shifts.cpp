376f327e502e27e4947c80584f8522e9c7841e51
Philipp Pereverzev <61697616+Phillie19@users.noreply.github.com>
2020-04-16 13:19:58 +0300

    Perform String Shifts.cpp

--- Perform String Shifts.cpp
+++ Perform String Shifts.cpp
@@ -3,19 +3,11 @@ public:
 	string stringShift(string s, vector<vector<int>>& shift) {
 		int len = s.size();
 		int pos = 0;
-		for (int i = 0; i < shift.size(); i++) {
-			pos = (shift[i][0] == 0) ? pos - shift[i][1] : pos + shift[i][1];
+		for (auto i : shift) {
+			pos = (i[0] == 0) ? pos - i[1] : pos + i[1];
 		}
 		pos = (pos > 0) ? len - pos % len : abs(pos) % len;
-		string ans;
-		for (int j = pos; j < len; j++) {
-			ans.push_back(s[j]);
-
-		}
-		for (int j = 0; j < pos; j++) {
-			ans.push_back(s[j]);
-
-		}
-		return ans;
+		rotate(s.begin(), s.begin() + pos, s.end());
+		return s;
 	}
 };
