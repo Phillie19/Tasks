e9a25a950c7a5a9a238eef2670151271ac043c99
Philipp Pereverzev <61697616+Phillie19@users.noreply.github.com>
2020-04-17 11:37:35 +0300

    Update Self Dividing Numbers.cpp

--- Self Dividing Numbers.cpp
+++ Self Dividing Numbers.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
 	vector<int> selfDividingNumbers(int left, int right) {
 		vector <int> ans;
-
 		for (int i = left; i <= right; i++) {
-			int num = i;
-			bool div = true;
-			while (num) {
-				int d = num % 10;
-				if (d == 0 || i % d != 0) {
-					div = false;
-					break;
-				}
-				num /= 10;
-			}
-			if (div) ans.push_back(i);
+			if (IsSelfDividing(i)) ans.push_back(i);
 		}
 		return ans;
+	}
+	bool IsSelfDividing(int num) {
+		int temp = num;
+		while (temp) {
+			int d = temp % 10;
+			if (d == 0 || num % d != 0) return false;
+			temp /= 10;
+		}
+		return true;
 	}
 };
