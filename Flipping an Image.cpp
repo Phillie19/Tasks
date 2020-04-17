ad6780e8987f8b18000c5daabf3820887b0cd2c0
Philipp Pereverzev <61697616+Phillie19@users.noreply.github.com>
2020-04-17 13:46:35 +0300

    Update Flipping an Image.cpp

--- Flipping an Image.cpp
+++ Flipping an Image.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
 	vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
 		for (auto& row : A) {
-			for (int i = 0, j = row.size() - 1; i < row.size() / 2; i++, j--) {
-				swap(row[i], row[j]);
-			}
+			reverse(row.begin(), row.end());
+
 			for (auto& pix : row) {
 				pix = !pix;
 			}
